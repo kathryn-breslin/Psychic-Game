6562000c2f1a8d781273851ab13c0d4dcfa772d2
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-01-31 11:35:13 -0500

    readme updates

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Psychic Game
+
+## About
 This game is intended to test your letter-guessing prowess against the computer!
 
 Begin by pressing any letter key. The computer will randomly generate a letter. 
@@ -7,6 +10,4 @@ guess the same letter, your loses increase.
 You have 10 attempts to guess the correct letter. If you don't guess the right letter
 in 10 attempts, you lose the game. 
 
-NOTE:
-
---This game is not yet fully functional--
+![Psychic Game](assets/images/psychic_game.png)
--- assets/javascript/gameRedo.js
+++ assets/javascript/gameRedo.js
@@ -1,19 +1,28 @@
+//lines 3 through 9 are initilizing these variables. They will only be initialized
+// on the first time the page loads.
+//What this means is you will have to think of a place to reset them in your onkeyup function
 var wins = 0;
 var losses = 0;
 var remainingGuesses = 10;
 var userGuesses = [];
 beginGame = false;
 endGame = false;
-
 var letters = ["k", "d", "y", "w"];
 
 document.onkeyup = function(event) {
-    
-    var userGuesses = event.key; 
 
+    var userGuesses = event.key;
+
+    //This will pick a new letter for the user to guess, every time the press a key
+    //This is not necessarily a bad thing, but I wasn't sure if this was your intention
+    //since it just makes the game super hard lol
     var computerGuesses = letters[Math.floor(Math.random() * letters.length)];
 
     if ((userGuesses) === (computerGuesses)) {
+        //Is your objective for this game to have the user see how many times they can
+        //guess a random letter in 10 tries?
+        //If not, then maybe this would a good place for the game to 'reset'
+        //i.e. pick a new letter, and reset the guesses
         wins++;
         remainingGuesses--;
         document.getElementById("wins").innerHTML = wins;
@@ -22,6 +31,7 @@ document.onkeyup = function(event) {
     }
 
     if ((userGuesses) != (computerGuesses)) {
+        //To me, at first glance, everything seems to be working correctly here.
         losses++;
         remainingGuesses--;
         document.getElementById("losses").innerHTML = losses;
@@ -29,6 +39,8 @@ document.onkeyup = function(event) {
         document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
     }
         if(remainingGuesses < 0) {
+          //This would probably be a good place to reset all of the variables as well as
+          //pick a new word
             alert("You went through your 10 tries!");
     }
 };
